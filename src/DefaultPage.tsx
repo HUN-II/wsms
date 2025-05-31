666f6891f1a61d9286115b76a29d9380abf7d587
HUNI_I <kchun0519@gmail.com>
2025-06-01 00:18:46 +0900

    Updates

--- src/DefaultPage.tsx
+++
@@ -1,9 +0,0 @@
-// DefaultPage: 날짜가 지정되지 않았을 때 기본으로 보여주는 페이지
-// DatePage를 '기본' 날짜로 렌더링합니다.
-import React from 'react';
-import DatePage from './DatePage';
-
-export default function DefaultPage() {
-  // DatePage에 date prop으로 '기본'을 전달
-  return <DatePage date={'기본'} />;
-} 
